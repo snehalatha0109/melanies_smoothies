d5c4d904031fc299f071a2bf853bfada45045b10
snehalatha0109 <spamz4644@gmail.com>
2026-06-22 22:46:53 +0530

    Add search functionality for selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(f"{fruit_chosen} Nutrition Information")
 
         smoothiefroot_response = requests.get(
