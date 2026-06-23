7c785a62f591b7a8d0915d22ceca04a2eadf0088
snehalatha0109 <spamz4644@gmail.com>
2026-06-23 13:04:10 +0530

    Refactor ingredients_string initialization
    Removed the creation of ingredients_string from ingredients_list and initialized it as an empty string.

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,11 @@ ingredients_list = st.multiselect(
 # Submit Section
 if ingredients_list:
 
-    ingredients_string = ", ".join(ingredients_list)
-
+    #ingredients_string = ", ".join(ingredients_list)
+    ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
-
+        ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
